afdc202071dfc592a5158cedd91143c93f5fc2d0
s-ahmed-01 <154647344+s-ahmed-01@users.noreply.github.com>
2025-02-13 21:26:21 +0000

    member intent

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 intents.reactions = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Create a scheduler
