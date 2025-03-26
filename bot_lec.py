225b12f5ab769e7894828f4eca7050f734005636
s-ahmed-01 <154647344+s-ahmed-01@users.noreply.github.com>
2025-03-26 02:25:14 +0000

    eh

--- bot_lec.py
+++ bot_lec.py
@@ -725,21 +725,21 @@ async def on_raw_reaction_add(payload):
                 # Generate score options based on match type
                 if match_type == 'BO1':
                     options = [f"{team1} wins", f"{team2} wins"]
-                    reactions = (REACTION_SETS[reaction_set][0], REACTION_SETS[reaction_set][5])  # Outer pair (🟦🟥)
+                    reactions = (REACTION_SETS[used_reaction_set][0], REACTION_SETS[used_reaction_set][5])  # Outer pair (🟦🟥)
                 elif match_type == 'BO3':
                     options = [f"{team1} 2-0", f"{team1} 2-1", f"{team2} 2-1", f"{team2} 2-0"]
                     reactions = (                                                    # From outside in:
-                        REACTION_SETS[reaction_set][0],  # 🟦 (far left)
-                        REACTION_SETS[reaction_set][1],  # 🟥 (far right)
-                        REACTION_SETS[reaction_set][4],  # 🔵 (inner left)
-                        REACTION_SETS[reaction_set][5]   # 🔴 (inner right)
+                        REACTION_SETS[used_reaction_set][0],  # 🟦 (far left)
+                        REACTION_SETS[used_reaction_set][1],  # 🟥 (far right)
+                        REACTION_SETS[used_reaction_set][4],  # 🔵 (inner left)
+                        REACTION_SETS[used_reaction_set][5]   # 🔴 (inner right)
                     )
                 elif match_type == 'BO5':
                     options = [
                         f"{team1} 3-0", f"{team1} 3-1", f"{team1} 3-2",
                         f"{team2} 3-2", f"{team2} 3-1", f"{team2} 3-0"
                     ]
-                    reactions = REACTION_SETS[reaction_set][:6]  # All six emojis
+                    reactions = REACTION_SETS[used_reaction_set][:6]  # All six emojis
 
                 # Now we can safely get the index
                 if str(payload.emoji.name) in reactions:  # Check if it's in the correct subset
